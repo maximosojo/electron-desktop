5571f4e1505982b12635f20a12dbd463bc06c0d9
Maximo Sojo <maxsojo13@gmail.com>
2019-12-22 11:29:27 -0400

    refactor(Init): Carga inicial de prueba electron js.

---
+++ main.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow} = require('electron');
+const macaddress = require('macaddress');
+
+const path = require("path");
+const url = require("url");
+
+let win;
+const basePath = "";
+
+function createWindow(){
+   	win = new BrowserWindow({width : 500, height : 500});
+      // win.maximize()
+      // win.show()
+   
+   // win.loadFile("index.html");
+   // win.loadURL(url.format({
+   // 	pathname: path.join(__dirname, 'index.html'),
+   // 	protocol: "file",
+   // 	slashes: true
+   // }))
+   	macaddress.one(function (err, mac) {
+   	   win.loadURL(basePath+mac);
+   	     console.log("Mac address for this host: %s", mac);  
+   	});
+}
+
+app.on("ready", createWindow);
